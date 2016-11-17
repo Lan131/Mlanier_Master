5a9a406255bdf172625d89701f091b351ed88f4f
mlanier <mlanier131@gmail.com>
2016-11-17 14:47:39 -0600

    Update darch_DBN.R

--- darch_DBN.R
+++ darch_DBN.R
@@ -7,6 +7,7 @@ library(caret)
 #autoencoder
 data(iris)
 summary(iris)
+
 darch <- darch(iris[,1:4], iris[,1:4], c(4,100,2,100,4), darch.dropout = .5,darch.isClass = F,
                preProc.params = list(method = c("center", "scale")), preProc.targets = T,
                darch.numEpochs = 100, darch.batchSize = 3,
@@ -22,21 +23,16 @@ ggplot(df, aes(df[,1])) +geom_histogram(aes(y = ..density..),bins=25) + geom_den
 
 ind=rowSums(abs(iris[,1:4]-predict(darch, newdata = iris[,1:4]))/iris[,1:4])
 dat=as.data.frame(cbind(iris,ind))
-data=dat[which(ind<1.5),]
+data=dat[which(ind<quantile(df[,1],.9)),]
 dat=data[,-6]
 
 
 
   #XOR
-dat=cbind(seq(1,nrow(dat),by=1),dat)
-trainIndex <- createDataPartition(dat[,1], p = .7, 
-                                   list = FALSE, 
-                                   times = 1)
- DataTrain <- dat[ trainIndex , ]
- DataTest  <- dat[-trainIndex,]
+
  
- 
-  tc <- trainControl(method = "boot", number = 5, allowParallel = F,
+
+  tc <- trainControl(method = "repeatedcv", number = 5, allowParallel = T,
                      verboseIter = T)
   
   parameters <- data.frame(parameter = c("layers", "bp.learnRate", "darch.unitFunction"),
@@ -52,18 +48,15 @@ trainIndex <- createDataPartition(dat[,1], p = .7,
     df
   }
   
-  darch <- train(Species ~ ., data = dat, tuneLength = 12, trControl = tc, 
+  darch <- train(Species ~ ., data = dat, tuneLength = 10, trControl = tc, 
                  method = darchModelInfo(parameters, grid), preProc = c("center", "scale"),
-                 darch.numEpochs = 15, darch.batchSize = 1000, testing = T)
+                 darch.numEpochs = 15, darch.batchSize = 10, testing = T)
   
   darch
   
-plot( darch)
+plot( darch$finalModel)
 ggplot(darch)
-
-
-
-
+darch$finalModel
 
 
 
@@ -90,10 +83,8 @@ darch <- darch(Hwt ~ Bwt,
 print(darchTest(darch, newdata = cats))
 
 
-
-
 plot( darch)
-ggplot(darch)
+
 
 
 
@@ -105,13 +96,9 @@ ggplot(darch)
 
 
 train=read.csv("C:\\Users\\0019091\\Desktop\\Tableau Reports\\ground.csv")
-trainIndex <- createDataPartition(train[,1], p = .7, 
-                                  list = FALSE, 
-                                  times = 1)
-DataTrain <- train[ trainIndex,]
-DataTest  <- train[-trainIndex,]
 
-tc <- trainControl(method = "boot", number = 5, allowParallel = F,
+
+tc <- trainControl(method = "repeatedcv", number = 5, allowParallel = T,
                    verboseIter = T)
 
 parameters <- data.frame(parameter = c("layers", "bp.learnRate", "darch.unitFunction"),
@@ -126,60 +113,15 @@ grid <- function(x, y, len = NULL, search = "grid")
   
   df
 }
-
-darch <- train(Target ~ Cat1+Cat2+Cont1, data = DataTrain, tuneLength = 12, trControl = tc, preProc.targets = T,
+attach(train)
+darch <- train(Target ~ Cat1+Cat2+Cont1, data = train, tuneLength = 12, trControl = tc,
                method = darchModelInfo(parameters, grid), preProc = c("center", "scale"), darch.isClass=F,
-              darch.numEpochs = 15, darch.batchSize = 10, xvalid=DataTest[,3:5],yvalid=DataTest[,2])
+              darch.numEpochs = 15, darch.batchSize = 10)
 
 darch
 
 
 plot( darch)
 ggplot(darch)
-
-darch <- darch(Target ~ Cat1+Cat2+Cont1, data = DataTrain,
-               preProc.params = list(method = c("center", "scale")),
-               preProc.targets = T,
-               layers = c(1,20,50,20,1),
-               darch.batchSize =  10,
-               bp.learnRate = .01,
-               darch.isClass = F,
-               darch.numEpochs = 100, xValid=DataTest[,2:5],yValid=DataTest[,2],
-               darch.unitFunction = linearUnit
-)
-
-
-
-
-
-
-data(iris)
-tc <- trainControl(method = "boot", number = 5, allowParallel = F,
-                   verboseIter = T)
-
-parameters <- data.frame(parameter = c("layers", "bp.learnRate", "darch.unitFunction"),
-                         class = c("character", "numeric", "character"),
-                         label = c("Network structure", "Learning rate", "unitFunction"))
-
-grid <- function(x, y, len = NULL, search = "grid")
-{
-  df <- expand.grid(layers = c("c(0,20,0)","c(0,10,10,0)","c(0,10,5,5,0)"), bp.learnRate = c(1,2,5,10))
-  
-  df[["darch.unitFunction"]] <- rep(c("c(tanhUnit, softmaxUnit)", "c(tanhUnit, tanhUnit, softmaxUnit)", "c(tanhUnit, tanhUnit, tanhUnit, softmaxUnit)"), 4)
-  
-  df
-}
-
-darch <- train(Species ~ ., data = iris, tuneLength = 12, trControl = tc, 
-               method = darchModelInfo(parameters, grid), preProc = c("center", "scale"),
-               darch.numEpochs = 15, darch.batchSize = 6, testing = T)
-
-darch
-
-plot( darch)
-ggplot(darch)
-
-
-
 
   
