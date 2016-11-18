442d45137f97649dc13f8ec937130b0f41f7adbb
mlanier <mlanier131@gmail.com>
2016-11-18 14:29:01 -0600

    Create caret_ensemble.R

---
+++ caret_ensemble.R
@@ -0,0 +1,82 @@
+library(caret)
+names(getModelInfo())
+?bagEarth
+getModelInfo()$glm$type
+
+
+library(RCurl)
+urlfile <-'https://raw.githubusercontent.com/hadley/fueleconomy/master/data-raw/vehicles.csv'
+x <- getURL(urlfile, ssl.verifypeer = FALSE)
+vehicles <- read.csv(textConnection(x))
+
+# alternative way of getting the data if the above snippet doesn't work:
+# urlData <- getURL('https://raw.githubusercontent.com/hadley/fueleconomy/master/data-raw/vehicles.csv')
+# vehicles <- read.csv(text = urlData)
+
+vehicles <- vehicles[names(vehicles)[1:24]]
+vehicles <- data.frame(lapply(vehicles, as.character), stringsAsFactors=FALSE)
+vehicles <- data.frame(lapply(vehicles, as.numeric))
+vehicles[is.na(vehicles)] <- 0
+vehicles$cylinders <- ifelse(vehicles$cylinders == 6, 1,0)
+
+
+prop.table(table(vehicles$cylinders))
+
+
+
+set.seed(1234)
+vehicles <- vehicles[sample(nrow(vehicles)),]
+split <- floor(nrow(vehicles)/3)
+ensembleData <- vehicles[0:split,]
+blenderData <- vehicles[(split+1):(split*2),]
+testingData <- vehicles[(split*2+1):nrow(vehicles),]
+
+
+labelName <- 'cylinders'
+predictors <- names(ensembleData)[names(ensembleData) != labelName]
+
+
+
+myControl <- trainControl(method='cv', number=3, returnResamp='none')
+
+
+test_model <- train(blenderData[,predictors], blenderData[,labelName], method='gbm', trControl=myControl)
+
+
+
+preds <- predict(object=test_model, testingData[,predictors])
+#install.packages('pROC')
+library(pROC)
+auc <- roc(testingData[,labelName], preds)
+print(auc$auc) 
+
+
+
+
+
+model_gbm <- train(ensembleData[,predictors], ensembleData[,labelName], method='gbm', trControl=myControl)
+
+model_rpart <- train(ensembleData[,predictors], ensembleData[,labelName], method='rpart', trControl=myControl)
+
+model_treebag <- train(ensembleData[,predictors], ensembleData[,labelName], method='treebag', trControl=myControl)
+
+
+
+blenderData$gbm_PROB <- predict(object=model_gbm, blenderData[,predictors])
+blenderData$rf_PROB <- predict(object=model_rpart, blenderData[,predictors])
+blenderData$treebag_PROB <- predict(object=model_treebag, blenderData[,predictors])
+
+testingData$gbm_PROB <- predict(object=model_gbm, testingData[,predictors])
+testingData$rf_PROB <- predict(object=model_rpart, testingData[,predictors])
+testingData$treebag_PROB <- predict(object=model_treebag, testingData[,predictors])
+
+
+
+predictors <- names(blenderData)[names(blenderData) != labelName]
+final_blender_model <- train(blenderData[,predictors], blenderData[,labelName], method='gbm', trControl=myControl)
+
+
+
+preds <- predict(object=final_blender_model, testingData[,predictors])
+auc <- roc(testingData[,labelName], preds)
+print(auc$auc)
