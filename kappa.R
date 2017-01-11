7f462ef1b81a9fe37d1d9cd790cb889a150ef07f
mlanier <mlanier131@gmail.com>
2017-01-11 14:52:27 -0600

    Create kappa.R

---
+++ kappa.R
@@ -0,0 +1,17 @@
+library(randomForest)
+library(caret)
+library(xgboost)
+
+
+data=read.csv("titanic.csv")
+data=na.roughfix(data)
+
+
+tc=trainControl(method="cv",number=5,search="random")
+
+xgb=train(as.factor(Survived)~as.factor(Pclass)+as.factor(Sex)+Age+SibSp+Fare+as.factor(Embarked),data=data,method="xgbTree",
+          na.action=na.omit,metric = "Kappa",#scale_pos_weight=4/5,
+          trControl=tc,preProc=c('center','scale'),tuneLength=15)
+
+xgb$results
+confusionMatrix(data=predict(xgb,data),reference=data$Survived)
