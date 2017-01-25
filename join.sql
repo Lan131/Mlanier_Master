f2d94dc48eb4ac6c129d075ddc930b4406f3cec4
mlanier <mlanier131@gmail.com>
2017-01-25 12:17:13 -0600

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,11 +1,8 @@
-select distinct T1.*,T2.cust_id,T2.order_date as future_date,T2.units_purchased as future_purchase,1
+
+select  T1.*,T2.cust_id,T2.order_date as future_date,T2.units_purchased as future_purchase,DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  as diff
 from Tab as T1 inner join Tab as T2 on T1.cust_id=T2.cust_id 
 where 
-
-T2.order_date >= DATEADD(dd, ((DATEDIFF(dd, '17530101', T1.order_date) / 7) * 7) + 7, '17530101') AND
-    T2.order_date <= DATEADD(dd, ((DATEDIFF(dd, '17530101', T1.order_date) / 7) * 7) + 13, '17530101') AND
-
-
+DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  =1 and
 
  T2.units_purchased<4 
 
