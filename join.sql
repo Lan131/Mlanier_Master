1ce3dcea1ee0cd97048bcbea5b3ef0110d57533a
mlanier <mlanier131@gmail.com>
2017-01-26 08:31:10 -0600

    Update join.sql

--- join.sql
+++ join.sql
@@ -5,3 +5,8 @@ DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  =1 and
 DATEPART ( yy , T2.order_date ) - DATEPART ( yy , T1.order_date )=0 and
 T2.units_purchased<4 
 order by T1.idx
+
+
+select * from Tab as T1 left join
+
+(select T1.cust_id as previous_cust,T1.items as previous_buy, 1 as returning from Tab as T1 inner join Tab as T2 on T1.cust_id=T2.cust_id where DATEDIFF(d,T1.order_date,T2.order_date)>0) as T2 on T1.idx=T2.idx
