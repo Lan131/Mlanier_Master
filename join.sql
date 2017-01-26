2f91cdeb9bcc86c66d61ecabde1ebe7c877e16f9
mlanier <mlanier131@gmail.com>
2017-01-25 20:46:30 -0600

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,9 +1,7 @@
-
-select  T1.*,T2.cust_id,T2.order_date as future_date,T2.units_purchased as future_purchase,DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  as diff
+select  T1.idx,T1.cust_id,T1.order_date as first,T2.order_date as second,DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date ) as diff,DATEPART ( year , T1.order_date ) as year_1, DATEPART ( yy , T2.order_date ) as year_2
 from Tab as T1 inner join Tab as T2 on T1.cust_id=T2.cust_id 
 where 
 DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  =1 and
-
- T2.units_purchased<4 
-
-
+DATEPART ( yy , T2.order_date ) - DATEPART ( yy , T1.order_date )=0 and
+T2.units_purchased<4 
+order by T1.idx
