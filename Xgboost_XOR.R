80accaa503743b4eeede96ea77b4e01936e3ac49
mlanier <mlanier131@gmail.com>
2017-01-27 10:50:54 -0600

    Update Xgboost_XOR.R

--- Xgboost_XOR.R
+++ Xgboost_XOR.R
@@ -1,7 +1,7 @@
 library(randomForest)
 library(caret)
 library(xgboost)
-
+library(AzureML)
 
 data=read.csv("titanic.csv")
 data=na.roughfix(data)
@@ -10,10 +10,39 @@ data$Survived[data$Survived==1] <- "yes"
 data$Survived[data$Survived==0] <- "no"
 tc=trainControl(method="cv",number=5,search="random",classProbs = TRUE)
 
+#ga_ctrl <- gafsControl(functions = rfGA,
+#                       method = "repeatedcv",
+#                       repeats = 5)
+#rf_ga <- gafs(x = as.data.frame(cbind(as.factor(data$Pclass),
+#                    as.factor(data$Sex),data$Age,data$SibSp,data$Fare,as.factor(as.factor(data$Embarked)))),
+#              y = as.factor(data$Survived),
+#              iters = 5,
+#              gafsControl = ga_ctrl)
+new=data[,-c(1,2,4,9,11,8)]
 xgb=train(Survived~as.factor(Pclass)+as.factor(Sex)+Age+SibSp+Fare+as.factor(Embarked),data=data,method="xgbTree",
           na.action=na.omit,metric = "kappa",
           trControl=tc,preProc=c('center','scale'),tuneLength=2)
+predict(newdata=new,object=xgb)
 
-xgb$results
-confusionMatrix(data=predict(xgb,data),reference=data$Survived)
-multiClassSummary(xgb)
+myID = "44c0e3ce99f545f1848919d25e7639d6"
+myAuth= "0d86dea367a64c298f60aa8587c5eb8e"
+
+XGB_function=function(data)
+  
+{
+  return(predict(newdata=new,object=xgb))
+  
+}
+  
+ws <- workspace(id=myID,auth=myAuth)
+
+
+firstWebService = publishWebService(
+  ws,
+  "xgbOnline",
+  list("Pclass" = "string", "Sex" = "string", "Age" = "float","SibSp" = "string", "Fare" = "float",
+       "Embarked" = "string" ),
+  myID,
+  myAuth
+)
+  
