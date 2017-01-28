201bba7e610ed96d7cc9a3d22ca14a216c846b72
mlanier <mlanier131@gmail.com>
2017-01-27 19:04:47 -0600

    Update join.sql

--- join.sql
+++ join.sql
@@ -1,41 +1,17 @@
-select  T1.idx,T1.cust_id,T1.order_date as first,T2.order_date as second,DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date ) as diff,DATEPART ( year , T1.order_date ) as year_1, DATEPART ( yy , T2.order_date ) as year_2
-from Tab as T1 inner join Tab as T2 on T1.cust_id=T2.cust_id 
-where 
-DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  =1 and
-DATEPART ( yy , T2.order_date ) - DATEPART ( yy , T1.order_date )=0 and
-T2.units_purchased<4 
-order by T1.idx
-
-select Tab.*,A.ct from Tab inner join
-(select  cust_id, count(*) as ct from Tab group by cust_id  ) as A on Tab.cust_id=A.cust_id
 
 USE [my_db]
 GO
 
-select Tab.*,A.ct from Tab left join
-(select  cust_id, count(*) as ct from Tab where order_date< '4/1/2015' group by cust_id  ) as A on Tab.cust_id=A.cust_id
+select distinct L.*,ISNULL(R.response,0) as response from
 
- where order_date> '4/1/2015'
+(select Tab.*,A.ct from Tab left join
+(select  cust_id, count(*) as ct from Tab where order_date< '9/17/2015' group by cust_id  ) as A on Tab.cust_id=A.cust_id
 
-SELECT
-     cost, idx,
-     (
-     SELECT
-         ISNULL( AVG(cost) AS moving_average, cust_id
-     FROM
-          Tab T2
-     WHERE
-          (
-               SELECT
-                    COUNT(*)
-               FROM
-                    Tab T3
-               WHERE
-                    date_column1 BETWEEN T1.order_date AND T2.order_date
-               group by cust_id
-          ) BETWEEN 1 AND 5 
-     ),0)
-FROM
-     Tab T1
-GROUP BY 
-     cust_id
+ where order_date> '9/17/2015') as L left join 
+
+ (select 1 as response,T1.idx
+from Tab as T1 inner join Tab as T2 on T1.cust_id=T2.cust_id 
+where 
+DATEPART ( week , T2.order_date )  -DATEPART ( week , T1.order_date )  =1 and
+DATEPART ( yy , T2.order_date ) - DATEPART ( yy , T1.order_date )=0 and
+T2.units_purchased<4) as R on L.idx=R.idx where L.order_date<'3/20/2016' order by L.idx
